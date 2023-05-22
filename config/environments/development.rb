9035d28aee640132da4edec6e090b1807b7dcfc2
SahoItayama <91715728+SahoItayama@users.noreply.github.com>
2023-05-22 15:06:24 +0000

    [add] topページのレイアウト

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "bca7e3a0aa354109aff6df84a90af22a.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
